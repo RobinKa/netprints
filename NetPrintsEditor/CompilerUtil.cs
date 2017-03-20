2ef2f3324e5e09cf9216536c9c242faeb17c0eeb
Torax <xtremegosugaming@gmail.com>
2017-03-20 02:29:45 +0100

    added function to compile a list of sources

--- NetPrintsEditor/CompilerUtil.cs
+++ NetPrintsEditor/CompilerUtil.cs
@@ -10,6 +10,16 @@ namespace NetPrintsEditor
 {
     class CompilerUtil
     {
+        public static CompilerResults CompileSources(string outputPath, 
+            IEnumerable<string> assemblies, IEnumerable<string> sources, bool generateExecutable=false)
+        {
+            CSharpCodeProvider csc = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters(assemblies.ToArray(), outputPath, true);
+            parameters.GenerateExecutable = generateExecutable;
+            CompilerResults results = csc.CompileAssemblyFromSource(parameters, sources.ToArray());
+            return results;
+        }
+
         public static CompilerResults CompileStringToLibrary(string sourceCode, string outputPath)
         {
             CSharpCodeProvider csc = new CSharpCodeProvider();
