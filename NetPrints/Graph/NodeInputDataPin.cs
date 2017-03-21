01325c0eab44712c5020124b37dc4acc3bc5d942
Torax <xtremegosugaming@gmail.com>
2017-03-21 21:32:59 +0100

    added events for when the incoming / outgoing pin of an input data pin / output exec pin changes

--- NetPrints/Graph/NodeInputDataPin.cs
+++ NetPrints/Graph/NodeInputDataPin.cs
@@ -4,20 +4,37 @@ using System.Runtime.Serialization;
 
 namespace NetPrints.Graph
 {
+    public delegate void InputDataPinIncomingPinChangedDelegate(
+        NodeInputDataPin pin, NodeOutputDataPin oldPin, NodeOutputDataPin newPin);
+
     [DataContract]
     public class NodeInputDataPin : NodeDataPin
     {
+        public event InputDataPinIncomingPinChangedDelegate IncomingPinChanged;
+
         [DataMember]
         public NodeOutputDataPin IncomingPin
         {
-            get;
-            set;
+            get => incomingPin;
+            set
+            {
+                if(incomingPin != value)
+                {
+                    var oldPin = incomingPin;
+                    
+                    incomingPin = value;
+
+                    IncomingPinChanged?.Invoke(this, oldPin, incomingPin);
+                }
+            }
         }
 
         public bool UsesUnconnectedValue
         {
             get => PinType.IsPrimitive;
         }
+
+        private NodeOutputDataPin incomingPin;
 
         [DataMember]
         public object UnconnectedValue
--- NetPrints/Graph/NodeOutputExecPin.cs
+++ NetPrints/Graph/NodeOutputExecPin.cs
@@ -2,11 +2,32 @@
 
 namespace NetPrints.Graph
 {
+    public delegate void OutputExecPinOutgoingPinChangedDelegate(
+        NodeOutputExecPin pin, NodeInputExecPin oldPin, NodeInputExecPin newPin);
+
     [DataContract]
     public class NodeOutputExecPin : NodeExecPin
     {
+        public event OutputExecPinOutgoingPinChangedDelegate OutgoingPinChanged;
+
         [DataMember]
-        public NodeInputExecPin OutgoingPin { get; set; }
+        public NodeInputExecPin OutgoingPin
+        {
+            get => outgoingPin;
+            set
+            {
+                if(outgoingPin != value)
+                {
+                    var oldPin = outgoingPin;
+                    
+                    outgoingPin = value;
+
+                    OutgoingPinChanged?.Invoke(this, oldPin, outgoingPin);
+                }
+            }
+        }
+
+        private NodeInputExecPin outgoingPin;
 
         public NodeOutputExecPin(Node node, string name)
             : base(node, name)
