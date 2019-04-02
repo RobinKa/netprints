2ac31361ed18111677d109bbb39337917a58b0b1
Robin Kahlow <robinkahlow@web.de>
2019-04-02 22:00:44 +0100

    fixed compatibility for old TargetType on VariableNode

--- NetPrints/Compatibility/VariableNode.cs
+++ NetPrints/Compatibility/VariableNode.cs
@@ -14,19 +14,34 @@ namespace NetPrints.Graph
         public OldVariable OldVariable
         {
             get => null;
-            private set
-            {
-                oldVariableName = value.Name;
-            }
+            private set => oldVariable = value;
         }
 
-        private string oldVariableName = null;
+        [DataMember(Name = "TargetType", IsRequired = false, EmitDefaultValue = false)]
+        [Obsolete]
+        public TypeSpecifier OldTargetType
+        {
+            get => null;
+            private set => oldTargetType = value;
+        }
+
+        private OldVariable oldVariable = null;
+        private TypeSpecifier oldTargetType = null;
 
         internal void FixOldVariable()
         {
-            if (oldVariableName != null)
+            if (oldVariable != null)
             {
-                Variable = Method.Class.Variables.FirstOrDefault(newVar => newVar.Name == oldVariableName)?.Specifier;
+                // Try to find the variable in the class
+                Variable = Method.Class.Variables.FirstOrDefault(newVar => newVar.Class.FullName == oldTargetType.Name && newVar.Name == oldVariable.Name)?.Specifier;
+
+                // If it was not found recreate it
+                if (Variable is null)
+                {
+                    Variable = new VariableSpecifier(oldVariable.Name, oldVariable.VariableType, oldVariable.Visibility,
+                        oldVariable.Visibility, oldTargetType, oldVariable.Modifiers);
+                    Variable.Visibility = oldVariable.Visibility;
+                }
             }
         }
     }
